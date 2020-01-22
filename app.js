e82e8c54eaabc96386174c0a462eb3879a914e74
Eric <romualdo.zapacademy.com>
2020-01-22 02:23:29 -0600

    Se agregan los roles, así como la verificación de contraseña y el campo de usuario verificado

--- app.js
+++ app.js
@@ -1,55 +1,74 @@
   
 function registrar(){
+  //verifica que la contraseña y su verificación coincidan para proceder a registrar
+  if(document.getElementById('contrasena').value!==document.getElementById('confirmaContrasena').value){
+    alert("Verifica la Contraseña");
+   }else
+ {
   var email = document.getElementById('email').value;
   var contrasena = document.getElementById('contrasena').value;
 
  firebase.auth().createUserWithEmailAndPassword(email, contrasena)
   .catch(function(error) {
-   // Handle Errors here.
+   // Mensajes de Error en Crear Usuario
     var errorCode = error.code;
     var errorMessage = error.message;
     alert(error.message);
-    // ...
   })
   .then(function(){
-    verificarEmail();
-//Prueba
-    creaBase();
+    enviarEmail();
+    writeUserData();
   })
 }
+}
 
-//Prueba
-function creaBase(){
+
+function writeUserData(){
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
+      // Si el usuario está activo
+      //var displayName = user.displayName;
       var email = user.email;
       var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
+      //var photoURL = user.photoURL;
+      //var isAnonymous = user.isAnonymous;
       var uid = user.uid;
-firebase.database().ref('bancoDeAnfitriones/'+ uid ).set({
-  email : email,
-  nombre : document.getElementById('nombre').value,
-  papellido : document.getElementById('pApellido').value,
-  sapellido : document.getElementById('sApellido').value,
-  tel : document.getElementById('tel').value,
-});
-}
+
+
+      //Asignar Rol
+        var esadmin = document.getElementById("esAdmin").checked;
+        var rol="";
+        if(esadmin===true){
+          rol="ADMIN";
+        }else{
+          rol="ANFITRION";
+        }
+
+        //Registrar Usuarios con Rol
+        firebase.database().ref('bancoDeAnfitriones/'+ uid ).set({
+          email : email,
+          nombre : document.getElementById('nombre').value.toUpperCase(),
+          papellido : document.getElementById('pApellido').value.toUpperCase(),
+          sapellido : document.getElementById('sApellido').value.toUpperCase(),
+          tel : document.getElementById('tel').value,
+          srol: rol,
+          verificado: emailVerified,
+
+        })
+      }
   }) 
 }
-function accesar(){
-  var emaila = document.getElementById('emaila').value;
-  var contrasenaa = document.getElementById('contrasenaa').value;
 
- firebase.auth().signInWithEmailAndPassword(emaila, contrasenaa)
+function accesar(){
+  var emailacceso = document.getElementById('emailAcceso').value;
+  var contrasenaacceso = document.getElementById('contrasenaAcceso').value;
+
+ firebase.auth().signInWithEmailAndPassword(emailacceso, contrasenaacceso)
   .catch(function(error) {
-   // Handle Errors here.
+   // Mensajes de error
     var errorCode = error.code;
     var errorMessage = error.message;
     alert(errorMessage);
-    // ...
   });
 }
 
@@ -62,13 +81,13 @@ function salir(){
   })
 }
 
-function verificarEmail(){
+function enviarEmail(){
   var user = firebase.auth().currentUser;
   user.sendEmailVerification().then(function() {
-  // Email sent.
+  // Se envía mensaje
   alert("Email Enviado")
 }).catch(function(error) {
-  // An error happened.
+  // Error al enviar mensaje
   alert("Ha Ocurrido un Error");
 });
 }
